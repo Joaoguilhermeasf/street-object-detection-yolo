87a7b325cdea1423cf9ca4c56341b0e563aa4336
João Guilherme <joaoguilherme762@gmail.com>
2026-03-31 16:02:37 -0300

    frame capture added

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('teste')
+import cv2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+url = os.getenv("URL")
+
+import cv2
+
+cap = cv2.VideoCapture(url, cv2.CAP_FFMPEG)
+print("Opened:", cap.isOpened())
+
+ret, frame = cap.read()
+
+if ret:
+    cv2.imwrite("frame.jpg", frame)
+    print("Frame capturado da URL 🚀")
+else:
+    print("Erro ao capturar frame da URL")
+
+cap.release()
